498c0ede807bbcd70b768e3f1b4200c961ae14ee
Sunil Prasad <153387687+sunilprregmi@users.noreply.github.com>
2024-08-18 22:44:49 +0545

    Update channels.dart
    Purged all previously available stations and added few Nepalese stations.

--- lib/services/channels.dart
+++ lib/services/channels.dart
@@ -41,141 +41,153 @@ class Channels {
 
   List<Channel> channelList = [
     Channel(
-        channelName: "ABC News",
-        link:
-            "https://lnc-abc-news.tubi.video/index.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://abcnews.go.com/Contact'),
+      channelName: "NTV National",
+      link: "https://neptv.guruusr.workers.dev/ntv/ntv/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "NTV National .",
+    ),
     Channel(
-        channelName: "Al Jazeera English",
-        link: "https://live-hls-web-aje.getaj.net/AJE/index.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://network.aljazeera.net/en/contact'),
+      channelName: "NTV Plus",
+      link: "https://neptv.guruusr.workers.dev/ntv/ntv-plus/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "NTV Plus .",
+    ),
     Channel(
-        channelName: "BBC News",
-        link: "http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/65d92a8c8b24c80008e285c0/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c26f5a2-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=9f1a48db-9022-43d1-bb25-ba665285192f",
-        source: Source.iptv,
-        contactpage: 'https://www.bbc.co.uk/contact/questions/getting-in-touch/contact-bbc'),
+      channelName: "NTV News",
+      link: "https://neptv.guruusr.workers.dev/ntv/ntv-news/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "NTV News .",
+    ),
     Channel(
-        channelName: "Bloomberg",
-        link: "https://bloomberg.com/media-manifest/streams/us.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.bloomberg.com/professional/support/'),
+      channelName: "NTV Kohalpur",
+      link: "https://neptv.guruusr.workers.dev/ntv/ntv-kohalpur/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "NTV Kohalpur .",
+    ),
     Channel(
-        channelName: "Bloomberg+",
-        link: "https://bloomberg.com/media-manifest/streams/phoenix-us.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.bloomberg.com/professional/support/'),
+      channelName: "NTV Itahari",
+      link: "https://neptv.guruusr.workers.dev/ntv/ntv-itahari/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "NTV Itahari .",
+    ),
     Channel(
-        channelName: "CBC News Network",
-        link:
-            "https://lnc-cbc2.tubi.video/master.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://cbchelp.cbc.ca/hc/en-ca/requests/new'),
+      channelName: "ABC News",
+      link:
+          "https://neptv.guruusr.workers.dev/tvtoday/abc-news-nepal/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "ABC News .",
+    ),
     Channel(
-        channelName: "CBS News",
-        link:
-            "https://cbsn-us.cbsnstream.cbsnews.com/out/v1/55a8648e8f134e82a470f83d562deeca/master.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.cbs.com/showfeedback/'),
+      channelName: "TV Today HD",
+      link:
+          "https://neptv.guruusr.workers.dev/tvtoday/tv-today-hd/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "TV Today HD .",
+    ),
     Channel(
-        channelName: "Channel News Asia",
-        link: "https://ythls.armelin.one/channel/UC83jt4dlz1Gjl58fzQrrKZg.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.channelnewsasia.com/contact-us'),
+      channelName: "TV One HD",
+      link: "https://neptv.guruusr.workers.dev/tvtoday/tv-one-hd/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "TV One HD .",
+    ),
     Channel(
-        channelName: "CNBC",
-        link: "https://www.youtube.com/embed/9NyxcX3rhQs?autoplay=1",
-        source: Source.youtube,
-        contactpage: 'https://help.cnbc.com/contact/'),
+      channelName: "TV World HD",
+      link:
+          "https://neptv.guruusr.workers.dev/tvtoday/tv-world-hd/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "TV World HD .",
+    ),
     Channel(
-        channelName: "DW English",
-        link:
-            "https://dwamdstream102.akamaized.net/hls/live/2015525/dwstream102/index.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://corporate.dw.com/en/contact/s-30606'),
+      channelName: "Krishi TV",
+      link: "https://neptv.guruusr.workers.dev/tvtoday/krishi-tv/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "Krishi TV .",
+    ),
     Channel(
-        channelName: "Euronews",
-        link:
-            "https://cdn.klowdtv.net/803B48A/n1.klowdtv.net/live3/euronews_720p/playlist.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.euronews.com/contact'),
+      channelName: "Indigenous TV",
+      link:
+          "https://neptv.guruusr.workers.dev/tvtoday/indigenous-tv/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "Indigenous TV .",
+    ),
     Channel(
-        channelName: "France 24 English",
-        link: "https://ythls.armelin.one/channel/UCQfwfsi5VrQ8yKZ-UWmAEFg.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.francemediasmonde.com/en/contact'),
+      channelName: "TV Filmy",
+      link: "https://neptv.guruusr.workers.dev/tvtoday/tv-filmy/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "TV Filmy .",
+    ),
     Channel(
-        channelName: "GB News",
-        link:
-            "https://live-gbnews.simplestreamcdn.com/live5/gbnews/bitrate1.isml/manifest.m3u8",
-        source: Source.iptv,
-        contactpage: "https://www.gbnews.com/about-us/contact-us"),
+      channelName: "BTV Business",
+      link:
+          "https://neptv.guruusr.workers.dev/tvtoday/business-tv/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "BTV Business .",
+    ),
     Channel(
-        channelName: "Global News",
-        link: "http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/62cbef9ebb857100072fc187/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=2c7a7bb2-35fc-11ef-a031-2b5d494037a2&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=28cf81a2-fc9d-48d7-8d50-3eebbcf68d91",
-        source: Source.iptv,
-        contactpage: 'https://globalnews.ca/pages/contact-us/'),
+      channelName: "Sagarmatha",
+      link:
+          "https://neptv.guruusr.workers.dev/tvtoday/sagarmatha-tv/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "Sagarmatha .",
+    ),
     Channel(
-        channelName: "i24 News",
-        link:
-            "https://bcovlive-a.akamaihd.net/6e3dd61ac4c34d6f8fb9698b565b9f50/eu-central-1/5377161796001/playlist-all_dvr.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.i24news.tv/en/contact'),
+      channelName: "Kantipur HD",
+      link:
+          "https://neptv.guruusr.workers.dev/kantipur/kantipur-hd/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "Kantipur HD .",
+    ),
     Channel(
-        channelName: "LiveNOW from FOX",
-        link: "https://lnc-fox-live-now.tubi.video/index.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.livenowfox.com/contact-us'),
+      channelName: "Nepal 1",
+      link: "https://neptv.guruusr.workers.dev/yupp/nepal-1/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "Nepal 1 .",
+    ),
     Channel(
-        channelName: "NBC News Now",
-        link:
-            "https://dai2.xumo.com/amagi_hls_data_xumo1212A-xumo-nbcnewsnow/CDN/master.m3u8",
-        source: Source.iptv,
-        contactpage:
-            'https://www.nbcnews.com/information/nbc-news-info/contact-us-n1232521'),
+      channelName: "Nepal Mandal",
+      link: "http://103.96.246.211:8000/main",
+      source: Source.iptv,
+      contactpage: "Nepal Mandal .",
+    ),
     Channel(
-        channelName: "Newsmax TV",
-        link: "https://nmxlive.akamaized.net/hls/live/529965/Live_1/index.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.newsmax.com/contact/'),
+      channelName: "Mithila TV UHD",
+      link: "http://150.107.205.212:1935/live/mithila/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "Mithila TV UHD .",
+    ),
     Channel(
-        channelName: "NHK World Japan",
-        link: "https://ythls.armelin.one/channel/UCSPEjw8F2nQDtmUKPFNF7_A.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www3.nhk.or.jp/nhkworld/en/contact/'),
+      channelName: "Capital TV HD",
+      link: "https://streaming.tvnepal.com:19360/capitaltv/capitaltv.m3u8",
+      source: Source.iptv,
+      contactpage: "Capital TV HD .",
+    ),
     Channel(
-        channelName: "Press TV",
-        link:
-            "https://cdnlive.presstv.ir/cdnlive/smil:cdnlive.smil/playlist.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.presstv.ir/aboutus/contactus'),
+      channelName: "Himalaya HD",
+      link:
+          "http://103.213.31.243:8081/livepull/ottHIMALAYA480.stream/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "Himalaya HD .",
+    ),
     Channel(
-        channelName: "Scripps News",
-        link:
-            "https://livetv-fa.tubi.video/scripps-news/playlist.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://support.scrippsnews.com/support/tickets/new'),
+      channelName: "Bodhi TV",
+      link:
+          "https://webtv-stream.nettv.com.np/broadcaster/Bodhi450.stream/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "Bodhi TV .",
+    ),
     Channel(
-        channelName: "The Guardian",
-        link: "https://rakuten-guardian-1-ie.samsung.wurl.tv/playlist.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.theguardian.com/help/contact-us'),
+      channelName: "Paryawaran TV",
+      link:
+          "https://webtv-stream.nettv.com.np:443/broadcaster/Paryawaran.stream/playlist.m3u8",
+      source: Source.iptv,
+      contactpage: "Paryawaran TV .",
+    ),
     Channel(
-        channelName: "Ticker",
-        link:
-            "https://cdn-uw2-prod.tsv2.amagi.tv/linear/amg01486-tickernews-tickernewsweb-ono/playlist.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://tickernews.co/contact-ticker/'),
-    Channel(
-        channelName: "TRT World",
-        link: "https://tv-trtworld.medya.trt.com.tr/master.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://www.trtworld.com/contact'),
-    Channel(
-        channelName: "Yahoo! Finance",
-        link: "https://d1ewctnvcwvvvu.cloudfront.net/playlist.m3u8",
-        source: Source.iptv,
-        contactpage: 'https://yahoo.uservoice.com/forums/382977'),
+      channelName: "Divyadarshan TV",
+      link:
+          "http://live.divyadarshantv.com/hls/stream.m3u8",
+      source: Source.iptv,
+      contactpage: "Divyadarshan TV .",
+    ),
   ];
 }
